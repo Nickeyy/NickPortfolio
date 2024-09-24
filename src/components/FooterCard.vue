c3fdce67a95468b085dcfd4bced2c66f457df1fa
Nick Durrer <nick@nicks-macbook-pro.home>
2024-09-24 12:56:18 +0200

    working on footer

---
+++ src/components/FooterCard.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center py-12">
+        <h1 class="font-bold text-2xl md:text-3xl text-gray-700 dark:text-white">Let's work together</h1>
+        <h2 class="text-lg md:text-1xl text-gray-500 dark:text-zinc-300 pt-4 md:pt-6">Bli bla blubi und ich bin doch efach supi!</h2>
+    </div>
+    <div class="w-full bg-gray-200 dark:bg-zinc-700 rounded-xl mt-0">
+        sojdfklsdlfj
+    </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import IntroCard from '../components/IntroCard.vue'
+import FooterCard from '../components/FooterCard.vue'
 </script>
 
 <template>
@@ -8,6 +9,7 @@ import IntroCard from '../components/IntroCard.vue'
       class="container sm mx-auto px-4 p-4 md:p-8 px-8 border-2 border-gray-200 dark:border-zinc-700 rounded-xl bg-white dark:bg-zinc-800 space-y-10 m-6"
       style="max-width: 660px;">
       <IntroCard />
+      <FooterCard />
     </div>
   </main>
 </template>
